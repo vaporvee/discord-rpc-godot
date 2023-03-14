4a96327e65334f9a0c7cbb8f9fb275f7cf24a5b3
vaporvee <yannik@vaporvee.com>
2023-03-14 18:11:22 +0100

    doesnt even work lol

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include "main.h"
 #include <godot_cpp/core/class_db.hpp>
 #include <godot_cpp/variant/utility_functions.hpp>
+#include "./discord-game-sdk-cpp/discord.h"
 
 using namespace godot;
+
+discord::Core* core{};
 
 void DiscordRPC::_bind_methods() {
 }
@@ -17,9 +20,16 @@ DiscordRPC::~DiscordRPC() {
 }
 
 void DiscordRPC::_ready() {
-   UtilityFunctions::print("Hello mom");
+    UtilityFunctions::print("HELLO MOM");
+    discord::Result res /*= discord::Core::Create(53908232506183680, DiscordCreateFlags_NoRequireDiscord, &core)*/;
+	//discord::Activity activity{};
+	//activity.SetState("Testing");
+	//activity.SetDetails("Fruit Loops");
+	//core->ActivityManager().UpdateActivity(activity, [](discord::Result res) {
+
+    //});
 }
 
 void DiscordRPC::_process(float delta) {
-    
+    //::core->RunCallbacks();
 }
