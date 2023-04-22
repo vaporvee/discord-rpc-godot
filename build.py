49f517b6a47c277ecb8dc91c4036ea0f59a03f0d
Yannik <80621863+vaporvee@users.noreply.github.com>
2023-04-22 21:29:47 +0200

    fix build.py

--- build.py
+++ build.py
@@ -1,17 +1,18 @@
 import os
 import sys
 
-yes = {'yes','y', 'ye', ''}
-no = {'no','n'}
+yes = {'yes', 'y', 'ye', ''}
+no = {'no', 'n'}
 
-if sys.argv[1]:
+if len(sys.argv) > 1:
     choice = sys.argv[1].removeprefix("-")
 else:
-    sys.stdout.write("Do you want to open only the built project instead of the Godot Editor after building? ([y]es/[n]o): ")
+    sys.stdout.write(
+        "Do you want to open only the built project instead of the Godot Editor after building? ([y]es/[n]o): ")
     choice = input().lower()
 if choice in yes:
-   os.system("scons && cd project && godot")
+    os.system("scons && cd project && godot")
 elif choice in no:
-   os.system("scons && godot project\project.godot")
+    os.system("scons && godot project\project.godot")
 else:
-   sys.stdout.write("Please respond with 'yes' or 'no'")
+    sys.stdout.write("Please respond with 'yes' or 'no'")
