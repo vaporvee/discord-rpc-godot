2a723a72b59760572e1d3548fa1cab5fde93ec39
Yannik <80621863+vaporvee@users.noreply.github.com>
2023-04-22 23:01:17 +0200

    value info in projects window

--- project/main.gd
+++ project/main.gd
@@ -4,5 +4,6 @@ func _ready():
 	DiscordSDK.app_id = 918857075105349632
 	DiscordSDK.details = "Made with GDExtension"
 	DiscordSDK.state = "This is a test from GDScript in Godot 4"
-
+	
 	DiscordSDK.refresh()
+	$Info.text = $Info.text.replace("{id}",str(DiscordSDK.app_id)).replace("{details}",DiscordSDK.details).replace("{state}",DiscordSDK.state)
