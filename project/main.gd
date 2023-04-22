eb8396fb9d7dc9d528360bc2c0761cbdd3030ac2
Yannik <80621863+vaporvee@users.noreply.github.com>
2023-04-22 22:34:25 +0200

    removed todo

--- project/main.gd
+++ project/main.gd
@@ -2,9 +2,7 @@ extends Node
 
 func _ready():
 	DiscordSDK.app_id = 918857075105349632
-	print(DiscordSDK.app_id)
 	DiscordSDK.details = "Made with GDExtension"
-	print(DiscordSDK.details)
-	DiscordSDK.state = "This is a test from GDScript in Godot 4" #TODO:Change functions to variables if possible
-	print(DiscordSDK.state)
+	DiscordSDK.state = "This is a test from GDScript in Godot 4"
+
 	DiscordSDK.refresh()
