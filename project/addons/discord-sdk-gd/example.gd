94a81c3ce78ccd3b4c2773e83a438918a72cd84e
Yannik <80621863+vaporvee@users.noreply.github.com>
2023-05-01 15:17:51 +0200

    updated example.gd

--- project/addons/discord-sdk-gd/example.gd
+++ project/addons/discord-sdk-gd/example.gd
@@ -1,17 +1,17 @@
 extends Node
 
 func _ready():
-	Discord_Activity.app_id = 1099618430065324082 # Application ID
-	print("Discord working: " + str(Discord_Activity.get_is_discord_working())) # A boolean if everything worked
-	Discord_Activity.details = "A demo activity by vaporvee#1231"
-	Discord_Activity.state = "Checkpoint 23/23"
+	DiscordSDK.app_id = 1099618430065324082 # Application ID
+	print("Discord working: " + str(DiscordSDK.get_is_discord_working())) # A boolean if everything worked
+	DiscordSDK.details = "A demo activity by vaporvee#1231"
+	DiscordSDK.state = "Checkpoint 23/23"
 	
-	Discord_Activity.large_image = "game" # Image key from "Art Assets"
-	Discord_Activity.large_image_text = "Try it now!"
-	Discord_Activity.small_image = "boss" # Image key from "Art Assets"
-	Discord_Activity.small_image_text = "Fighting the end boss! D:"
+	DiscordSDK.large_image = "game" # Image key from "Art Assets"
+	DiscordSDK.large_image_text = "Try it now!"
+	DiscordSDK.small_image = "boss" # Image key from "Art Assets"
+	DiscordSDK.small_image_text = "Fighting the end boss! D:"
 	
-	Discord_Activity.start_timestamp = int(Time.get_unix_time_from_system()) # "02:41 elapsed"
+	DiscordSDK.start_timestamp = int(Time.get_unix_time_from_system()) # "02:41 elapsed"
 	# Discord_Activity.end_timestamp = int(Time.get_unix_time_from_system()) + 3600 # +1 hour in unix time
 
-	Discord_Activity.refresh() # Always refresh after changing the values!
+	DiscordSDK.refresh() # Always refresh after changing the values!
