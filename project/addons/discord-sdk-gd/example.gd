25ec5699cf0dcac1c1bb4bb67d09586621c94bf2
Yannik <80621863+vaporvee@users.noreply.github.com>
2023-05-01 16:05:14 +0200

    updated example.gd

--- project/addons/discord-sdk-gd/example.gd
+++ project/addons/discord-sdk-gd/example.gd
@@ -6,18 +6,18 @@ extends Node
 # 4. Create an Application under https://discord.com/developers/applications and get the Application ID
 # 5. (optional) Set images under "Rich Presence" and "Art Assets" and remember the keys
 
-func _ready():
-	DiscordSDK.app_id = 1099618430065324082 # Application ID
-	print("Discord status: " + DiscordSDK.get_sdk_info()) # Returns "Ok" if everything worked
-	DiscordSDK.details = "A demo activity by vaporvee#1231"
-	DiscordSDK.state = "Checkpoint 23/23"
+#func _ready():
+	#DiscordSDK.app_id = 1099618430065324082 # Application ID
+	#print("Discord working: " + str(Discord_Activity.get_is_discord_working())) # A boolean if everything worked
+	#DiscordSDK.details = "A demo activity by vaporvee#1231"
+	#DiscordSDK.state = "Checkpoint 23/23"
 	
-	DiscordSDK.large_image = "game" # Image key from "Art Assets"
-	DiscordSDK.large_image_text = "Try it now!"
-	DiscordSDK.small_image = "boss" # Image key from "Art Assets"
-	DiscordSDK.small_image_text = "Fighting the end boss! D:"
+	#DiscordSDK.large_image = "game" # Image key from "Art Assets"
+	#DiscordSDK.large_image_text = "Try it now!"
+	#DiscordSDK.small_image = "boss" # Image key from "Art Assets"
+	#DiscordSDK.small_image_text = "Fighting the end boss! D:"
 	
-	DiscordSDK.start_timestamp = int(Time.get_unix_time_from_system()) # "02:41 elapsed"
+	#DiscordSDK.start_timestamp = int(Time.get_unix_time_from_system()) # "02:41 elapsed"
 	# Discord_Activity.end_timestamp = int(Time.get_unix_time_from_system()) + 3600 # +1 hour in unix time
 	
-	DiscordSDK.refresh() # Always refresh after changing the values!
+	#DiscordSDK.refresh() # Always refresh after changing the values!
