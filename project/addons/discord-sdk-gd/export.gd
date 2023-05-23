da42d6a22894296dbb37e687686688d549d23a87
Bioblaze Payne <BioblazePayne@gmail.com>
2023-05-23 11:48:50 -0700

    Update export.gd to support extend environments
    After having a conversation with Yannik, had more environment variables in my array then most so made this lil change to make it work. Works flawlessly.

--- project/addons/discord-sdk-gd/export.gd
+++ project/addons/discord-sdk-gd/export.gd
@@ -2,16 +2,17 @@
 extends  EditorExportPlugin
 
 func _export_file(_path, _type, features) -> void:
-	if features[2] == "windows":
-		if features[4] == "x86_64":
-			add_shared_object("res://addons/discord-sdk-gd/bin/windows/discord_game_sdk.dll",[],"/")
-		elif features[4] == "x86":
-			add_shared_object("res://addons/discord-sdk-gd/bin/windows/discord_game_sdk_x86.dll",[],"/")
-	elif features[2] == "linux":
-		add_shared_object("res://addons/discord-sdk-gd/bin/linux/libdiscord_game_sdk.so",[],"/")
-	elif features[2] == "macos":
-		add_shared_object("res://addons/discord-sdk-gd/bin/macos/libdiscord_game_sdk.dylib",[],"/")
-		add_shared_object("res://addons/discord-sdk-gd/bin/macos/libdiscord_game_sdk_aarch64.dylib",[],"/")
+    print("FEATURES: ", features)
+    if features.find("windows",0) != -1:
+        if features.find("x86_64",0) != -1:
+            add_shared_object("res://addons/discord-sdk-gd/bin/windows/discord_game_sdk.dll",[],"/")
+        elif features.find("x86",0) != -1:
+            add_shared_object("res://addons/discord-sdk-gd/bin/windows/discord_game_sdk_x86.dll",[],"/")
+    elif features.find("linux",0) != -1:
+        add_shared_object("res://addons/discord-sdk-gd/bin/linux/libdiscord_game_sdk.so",[],"/")
+    elif features.find("macos",0) != -1:
+        add_shared_object("res://addons/discord-sdk-gd/bin/macos/libdiscord_game_sdk.dylib",[],"/")
+        add_shared_object("res://addons/discord-sdk-gd/bin/macos/libdiscord_game_sdk_aarch64.dylib",[],"/")
 
 func _get_name():
-	pass
+    pass
