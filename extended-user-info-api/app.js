0ee6a3268c66404ac48dd53564e8fd8ce03b5665
Yannik <80621863+vaporvee@users.noreply.github.com>
2023-08-14 00:14:01 +0200

    removed weird urls with null values

--- extended-user-info-api/app.js
+++ extended-user-info-api/app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const request = require('request');
 const express = require('express');
+const e = require('express');
 
 var port = process.env.PORT || 1187;
 var botToken = process.env.BOT_TOKEN;
@@ -34,17 +35,22 @@ app.get("/*", (a, response) => {
                 "public_flags": preuser.public_flags,
                 "flags": preuser.flags,
                 "accent_color": preuser.accent_color,
-                "avatar_decoration_url": "https://cdn.discordapp.com/avatar-decorations/" + preuser.id + "/" + preuser.avatar_decoration + ".png",
                 "avatar_decoration": preuser.avatar_decoration,
-                "banner_url": "https://cdn.discordapp.com/banners/" + preuser.id + "/" + preuser.banner + ".png",
                 "banner": preuser.banner,
                 "banner_color": preuser.banner_color,
             };
+            if (preuser.avatar_decoration == null)
+                user.avatar_decoration = null;
+            else
+                user.avatar_decoration = "https://cdn.discordapp.com/avatar-decorations/" + preuser.id + "/" + preuser.avatar_decoration + ".png";
+            if (preuser.banner == null)
+                user.banner_url = null;
+            else
+                user.banner_url = "https://cdn.discordapp.com/banners/" + preuser.id + "/" + preuser.banner + ".png";
             response.send(user);
         }
-        else {
+        else
             response.send(aresponse.statusCode)
-        }
     }
 
     request(options, callback);
