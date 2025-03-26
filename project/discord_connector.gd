bd7a102d3d97b3d1c7e9322726664ff6bba88b06
vaporvee <yannik@vaporvee.com>
2025-03-26 22:33:57 +0100

    kind of fixed the lobby

--- project/discord_connector.gd
+++ project/discord_connector.gd
@@ -3,12 +3,6 @@ extends DiscordConnector
 func _on_user_connected(access_token: String, refresh_token: String, expires_in: int) -> void:
 	print_debug("Access token: %s \nRefresh Token: %s \nExpires in: %s" % [access_token, refresh_token, expires_in])
 
-func _on_user_updated() -> void:
-	print_debug("USER UPDATED")
-	await get_tree().create_timer(2).timeout
-	print("oop")
-	$DiscordLobby.create_or_join_lobby("beanis")
-
 func _on_user_connection_failed(error: String) -> void:
 	push_error("User connection failed! Error: " + error)
 
@@ -17,6 +11,7 @@ func _on_user_update_failed(error: String) -> void:
 
 func _on_connection_ready() -> void:
 	print_debug("CONNECTION READY")
+	$DiscordLobby.create_or_join_lobby("beanis")
 
 func _on_connection_error(error: String) -> void:
 	push_error(error)
--- src/nodes/discord_lobby.cpp
+++ src/nodes/discord_lobby.cpp
@@ -5,27 +5,21 @@ void DiscordLobby ::_bind_methods()
     BIND_METHOD(DiscordLobby, create_or_join_lobby, "secret");
 }
 
-void DiscordLobby::create_or_join_lobby(String secret) {
-    if (connector) {
-        auto current_client = connector->client;
-
-        current_client->CreateOrJoinLobby(secret.utf8().get_data(),[current_client](discordpp::ClientResult result, uint64_t lobbyId) {
+void DiscordLobby::create_or_join_lobby(String secret)
+{
+    DiscordLobby::connector->client->CreateOrJoinLobby(secret.utf8().get_data(), [this](discordpp::ClientResult result, uint64_t lobbyId)
+                                                       {
             if(result.Successful()) {
             std::cout << "Lobby created or joined successfully! Lobby Id: " << lobbyId << std::endl;
             } else {
-            std::cout << "Lobby creation/join failed\n" << result.Error().c_str();
-            }
-        });
-    }
+            UtilityFunctions::print("Failed to create or join lobby: " + String(result.Error().c_str()));
+            } });
 }
 
 DiscordLobby::DiscordLobby()
 {
-
 }
 
 DiscordLobby::~DiscordLobby()
 {
-
 }
-
